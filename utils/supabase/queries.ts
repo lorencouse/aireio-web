d819c2e7e85a526b17427e519bc9f2a0fddead1a
“Loren <couselm@gmail.com>
2024-08-28 15:43:01 +0800

    Merge with main

--- utils/supabase/queries.ts
+++ utils/supabase/queries.ts
@@ -63,15 +63,3 @@ export const getSubscription = cache(async (supabase: SupabaseClient) => {
 //     .single();
 //   return userDetails;
 // });
-
-// export const getProducts = cache(async (supabase: SupabaseClient) => {
-//   const { data: products, error } = await supabase
-//     .from('products')
-//     .select('*, prices(*)')
-//     .eq('active', true)
-//     .eq('prices.active', true)
-//     .order('metadata->index')
-//     .order('unit_amount', { referencedTable: 'prices' });
-
-//   return products;
-// });
