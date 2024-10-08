d11af9c0272b62891afe495231c676e61f561d26
“Loren <couselm@gmail.com>
2024-10-08 13:00:54 +0800

    Changes to cookies for SSR

--- utils/supabase/queries.ts
+++ utils/supabase/queries.ts
@@ -1,39 +1,18 @@
-import { SupabaseClient } from '@supabase/supabase-js';
-import { UserProfile } from '../types';
-import { createClient } from '@supabase/supabase-js';
-
-import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
-import { cookies } from 'next/headers';
+import { createClient } from '@/utils/supabase/server';
 import { cache } from 'react';
 import { User } from '@supabase/supabase-js';
 import { Database } from '@/types_db';
 
 export const getUser = cache(async () => {
-  const supabase = createServerComponentClient({ cookies });
+  const supabase = createClient();
   const {
     data: { user }
   } = await supabase.auth.getUser();
   return user;
 });
 
-// export const getUser = cache(async () => {
-//   const {
-//     data: { user, error }
-//   } = await supabase.auth.getUser();
-
-//   if (error) {
-//     console.log('Could not get user', error);
-//     return null;
-//   }
-
-//   return user;
-// });
-
 export const getUserProfile = async () => {
-  const supabase = createClient<Database>(
-    process.env.NEXT_PUBLIC_SUPABASE_URL || '',
-    process.env.SUPABASE_SERVICE_ROLE_KEY || ''
-  );
+  const supabase = createClient();
   const user: User | null = await getUser();
 
   if (!user) return null;
