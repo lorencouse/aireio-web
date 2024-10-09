ee79dfbf552e1d99561a271a840ee7fb73f2283d
“Loren <couselm@gmail.com>
2024-10-09 14:16:30 +0800

    Add not-found page

---
+++ app/not-found.tsx
@@ -0,0 +1,10 @@
+export const runtime = 'edge';
+
+export default function NotFound() {
+  return (
+    <div>
+      <h2>Not Found</h2>
+      <p>Could not find the requested resource</p>
+    </div>
+  );
+}
