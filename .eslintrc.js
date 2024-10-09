eaef1fda482daaa13b9bb9945ace6716bb5c5f06
“Loren <couselm@gmail.com>
2024-10-09 15:29:46 +0800

    changes to package.json again

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,9 +2,11 @@ module.exports = {
   extends: [
     'next/core-web-vitals',
     'prettier',
-    'plugin:eslint-plugin-next-on-pages/recommended'
+    'plugin:eslint-plugin-next-on-pages/recommended',
+    'plugin:@typescript-eslint/recommended'
   ], // Use Next.js and Prettier defaults
-  plugins: ['tailwindcss', 'eslint-plugin-next-on-pages'], // Enable Tailwind CSS linting
+  plugins: ['tailwindcss', 'eslint-plugin-next-on-pages', '@typescript-eslint'], // Enable Tailwind CSS linting
+  parser: '@typescript-eslint/parser',
   rules: {
     // Turn off or adjust rules as needed
     // '@typescript-eslint/no-explicit-any': 'off',
