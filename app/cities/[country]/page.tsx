c588dbcdf4e28290ed6a3927429125f21539b8f8
“Loren <couselm@gmail.com>
2024-10-15 14:17:38 +0800

    Fix bug preventing redirect after login.

--- app/cities/[country]/page.tsx
+++ app/cities/[country]/page.tsx
@@ -24,8 +24,8 @@ export default async function CountryPage({
 }: {
   params: { country: string; state: string };
 }) {
-  const { country, state } = params;
-  const cities: City[] = await getCities(country, state);
+  const { country } = params;
+  const cities: City[] = await getCities(country);
 
   return (
     <div className="flex flex-col justify-center items-center w-full mt-[1rem]">
